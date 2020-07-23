4418f3b0807c0a55c126f30ee5f492d68f8773d5
Brajan Osiński <brajan.osinski@gmail.com>
2020-07-23 17:23:31 +0200

    add function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  }
+  else if(argMoveId == 2){
+    return 'papier';
+  }
+  else if(argMoveId == 3){
+    return 'nożyce';
+  }
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 ​
-let computerMove = 'nieznany ruch';
+
+let computerMove = getMoveName(randomNumber);
 ​
-if(randomNumber == 1){
+*if(randomNumber == 1){
   computerMove = 'kamień';
 }
 else if(randomNumber == 2){
@@ -13,8 +27,7 @@ else if(randomNumber == 2){
   else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
-​let playerMove = 'nieznany ruch';
-
+let playerMove = getMoveName(randomNumber);
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 if(playerInput == '1'){
